8acdc0b0578415d78f6d658306ce342ff31d1d25
Ankur Dubey <dubeyankur@gmail.com>
2016-12-08 11:53:49 +0530

    First Checkin

---
+++ WineOutlet_1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace WineOutlet_1
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new WineOutlet_1Page("https://www.wineoutlet.com/");
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
